09236e8d12d7a5b95af050da640dab6298b2015c
Test <test@MacBook-Pro-de-Test.local>
2022-12-08 20:12:04 -0600

    Empezamos a modificar estilos con CSS en React

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-import React from 'react'
-import { TodoCounter } from './TodoCounter'
+import React from 'react';
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from './TodoSearch';
+import { TodoList } from './TodoList';
+import { TodoItem } from './TodoItem';
+import { CreateTodoButton } from './CreateTodoButton';
+
 // import './App.css';
 
 const todos = [
   { text: 'Cortar cebolla', completed: false},
   { text: 'Tomar curso intro React', completed: false},
   { text: 'Llorar con la llorona', completed: false},
+  { text: 'Tomar Curso Orientación Vocacional', completed: false}
 ]
 
 function App() {
   return (
     <React.Fragment>
       <TodoCounter />
-      {/*<TodoSearch />*/}
+      <TodoSearch />
         <input placeholder="Ingresa tu búsqueda"/>
-      {/*<TodoList>
+      <TodoList>
         {todos.map(todo => (
-          <TodoItem />
+          <TodoItem 
+          key={todo.text} 
+          text = {todo.text}/>
           )
         )}
-        </TodoList>*/}
-      {/*<CreateTodoButton />*/}
-        <button>
-        +
-        </button>
+        </TodoList>
+      <CreateTodoButton />
     </React.Fragment>
 
   );
